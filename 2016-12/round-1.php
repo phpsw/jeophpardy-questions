5ae0b8591889ce73c5e9f9a8ff058b523de6c3c7
Lee Stone <lee@leestone.co.uk>
2017-11-28 11:55:45 +0000

    Add question rounds from Christmas 2016

---
+++ 2016-12/round-1.php
@@ -0,0 +1,105 @@
+<?php return [
+	'pointScale' => 200,
+	'categories' => [
+		[
+			'name' => 'PHP &nbsp; 7',
+			'questions' =>  [
+				[
+				 	'question' => 'This new interface allows you to catch both Errors and Exceptions in your code.',
+				 	'answer'   => 'What is the Throwable interface?',
+				],
+				[
+				 	'question' => 'This operator allows you to perform a three-way comparison in one go.',
+				 	'answer'   => 'What is a spaceship operator?',
+				],
+				[
+				 	'question' => 'This RFC which made its way into PHP 7.1 allows you to specify that a function should not return anything.',
+				 	'answer'   => 'What is a void return type?',
+				],
+				[
+				 	'question' => 'This operator will return the left operand if it is not null, otherwise it will return the right. ',
+				 	'answer'   => 'What is the null coalesce operator?',
+				],
+				[
+				 	'question' => 'This RFC which made its way into PHP 7.1 allows you to declare that your function may accept and/or return nothing',
+				 	'answer'   => 'What is/are nullable types?',
+				],
+			]
+		],
+		[
+			'name' => 'PHPSW',
+			'questions' =>  [
+				[
+				 	'question' => 'From the length of his beard, we know this speaker must be wise',
+				 	'answer'   => 'Who is Dan Ackroyd?',
+				],
+				[
+				 	'question' => 'David McKay spoke to us about this software containerization platform',
+				 	'answer'   => 'What is Docker?',
+				],
+				[
+				 	'question' => 'Drew McLellan spoke to us about a security issue in which CMS he creates?',
+				 	'answer'   => 'What is Perch?',
+				],
+				[
+				 	'question' => 'This month is when we finally got a talk about PHP 7',
+				 	'answer'   => 'When is May?',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+			]
+		],
+		[
+			'name' => 'Packaged',
+			'questions' =>  [
+				[
+				 	'question' => 'This package is a favourite for handling logging',
+				 	'answer'   => 'What is monolog?',
+				],
+				[
+				 	'question' => 'This package is popular for abstracting local and remote filesystems',
+				 	'answer'   => 'What is Flysystem?',
+				],
+				[
+				 	'question' => 'This dependency injector is frequently mentioned by Dan Ackroyd',
+				 	'answer'   => 'What is Auryn',
+				],
+				[
+				 	'question' => 'This is the name of a group of packages brought together by cider drinker Phil Sturgeon',
+				 	'answer'   => 'The League of Extraordinary Packages',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+			]
+		],
+		[
+			'name' => 'Bristol Pubs',
+			'questions' =>  [
+				[
+				 	'question' => 'You\'ll commonly find PHPSW here after a meetup',
+				 	'answer'   => 'What is the Volunteer Tavern?',
+				],
+				[
+				 	'question' => 'This pub is on a boat and is popular with cider drinkers',
+				 	'answer'   => 'What is The Apple?',
+				],
+				[
+				 	'question' => 'This pub claims to be Bristol\'s oldest',
+				 	'answer'   => 'What is The Hatchet?',
+				],
+				[
+				 	'question' => 'This pub has recently been reopened after it was closed last year after its 3rd shooting in 6 months',
+				 	'answer'   => 'What is The Jolly Roger in Easton ',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+			]
+		],
+	]
+];
