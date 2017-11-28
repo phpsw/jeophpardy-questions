d80a0b303125c79408cb89d16ab38416005def88
Kat <kasia.zien@gmail.com>
2017-11-28 23:59:01 +0000

    Add 2017 dir

---
+++ 2017-12/round-2.php
@@ -0,0 +1,105 @@
+<?php return [
+	'pointScale' => 200,
+	'categories' => [
+		[
+			'name' => 'Elephpants',
+			'questions' =>  [
+				[
+				 	'question' => 'This elephpant is helping promote diversity',
+				 	'answer'   => 'Who is enfys / What is the rainbow elephpant?',
+				],
+				[
+				 	'question' => 'The woolly Mammoth was created as a mascot for this PHP Conference',
+				 	'answer'   => 'What is trueNorth PHP?',
+				],
+				[
+				 	'question' => 'The elephpant was first sketched in 1998 by this person',
+				 	'answer'   => 'Who is Vincent Pontier?',
+				],
+				[
+				 	'question' => 'This is the name of PHP Architect\'s Orange elephpant',
+				 	'answer'   => 'Who is Archie?',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+			]
+		],
+		[
+			'name' => 'Community Conferences',
+			'questions' =>  [
+				[
+				 	'question' => 'This UK conference starts out with no schedule',
+				 	'answer'   => 'What is the PHPEM Un Con/',
+				],
+				[
+				 	'question' => 'If you\'ve made it up for the 9am Sunday morning hangover slot, you must be at what conference',
+				 	'answer'   => 'What is PHPNW?',
+				],
+				[
+				 	'question' => 'This conference takes place in Action Stations',
+				 	'answer'   => 'What is PHP South Coast?',
+				],
+				[
+				 	'question' => 'This conference had bumper cars at its social',
+				 	'answer'   => 'What is PHP Benelux?',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+			]
+		],
+		[
+			'name' => 'Name the namespace',
+			'questions' =>  [
+				[
+				 	'question' => 'This library allows you to specify your dependencies in a JSON file.',
+				 	'answer'   => 'What is Composer?',
+				],
+				[
+				 	'question' => 'This framework comes with an artisan command line tool.',
+				 	'answer'   => 'What is Laravel?',
+				],
+				[
+				 	'question' => 'This static site generator created by Beau Simensen is named after a fish.',
+				 	'answer'   => 'What is Sculpin?',
+				],
+				[
+				 	'question' => 'This PHP framework uses the Gherkin language to describe the behaviour of an application. ',
+				 	'answer'   => 'What is Behat?',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+			]
+		],
+		[
+			'name' => 'Bristol',
+			'questions' =>  [
+				[
+				 	'question' => 'Bristol is situated on these rivers',
+				 	'answer'   => 'What are the Frome and Avon?',
+				],
+				[
+				 	'question' => 'This scene from Only Fools and Horses was filmed in Broadmead',
+				 	'answer'   => 'The famous batman and robin scene',
+				],
+				[
+				 	'question' => 'This person sailed from Bristol to help discover North America',
+				 	'answer'   => 'Who is John Cabbot',
+				],
+				[
+				 	'question' => 'This drink was invented by at Bristol University',
+				 	'answer'   => 'What is Ribena',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+			]
+		],
+	]
+];
